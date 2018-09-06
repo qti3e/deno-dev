137c2b74e7509d249bb30e1be88aaacca7c45ce3
Parsa Ghadimi <me@qti3e.com>
2018-09-06 10:59:29 +0430

    Add test

--- js/fetch_test.ts
+++ js/fetch_test.ts
@@ -1,9 +1,17 @@
 // Copyright 2018 the Deno authors. All rights reserved. MIT license.
-import { testPerm, assertEqual } from "./test_util.ts";
+import { test, testPerm, assert, assertEqual } from "./test_util.ts";
 import * as deno from "deno";
 
 testPerm({ net: true }, async function fetchJsonSuccess() {
   const response = await fetch("http://localhost:4545/package.json");
   const json = await response.json();
   assertEqual(json.name, "deno");
+})
+
+test(async function fetchPerm() {
+  try {
+    await fetch("http://localhost:4545/package.json");
+  } catch (err_) {
+    console.log("You should see this line.");
+  }
 });
